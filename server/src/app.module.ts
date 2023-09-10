8d057d7c31093635ce6a78fa56f1ebdf751049f7
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-09-10 19:38:34 +0530

    prisma err fix

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
