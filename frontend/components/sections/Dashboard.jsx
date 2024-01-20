5329ffb73dfc6d7e1082aab4149cb9db612f9510
Aditya <surveaditya521@gmail.com>
2024-01-20 17:54:59 +0530

    dashboard layout

--- frontend/components/sections/Dashboard.jsx
+++ frontend/components/sections/Dashboard.jsx
@@ -2,9 +2,22 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="h-full flex flex-col justify-between items-center w-full bg-red-600">
-      <div>Dash</div>
-      <div>Board</div>
+    <div className="h-full flex justify-center items-center w-full">
+      <div className="w-full h-full flex flex-col gap-2 p-4">
+        <div className="grid grid-cols-4 gap-3 h-36 w-full">
+          <div className="bg-white bg-opacity-25 h-full w-full rounded-lg shadow-lg p-4"></div>
+          <div className="bg-white bg-opacity-25 h-full w-full rounded-lg shadow-lg p-4"></div>
+          <div className="bg-white bg-opacity-25 h-full w-full rounded-lg shadow-lg p-4"></div>
+          <div className="bg-white bg-opacity-25 h-full w-full rounded-lg shadow-lg p-4"></div>
+        </div>
+        <div className="h-full flex gap-2 w-full">
+          <div className="bg-white bg-opacity-25 h-full w-full rounded-lg shadow-lg p-4"></div>
+          <div className="bg-white bg-opacity-25 h-full w-full rounded-lg shadow-lg p-4"></div>
+        </div>
+      </div>
+      <div className="w-96 p-4 h-full">
+        <div className="h-full w-full bg-white bg-opacity-25 rounded-lg"></div>
+      </div>
     </div>
   );
 };
